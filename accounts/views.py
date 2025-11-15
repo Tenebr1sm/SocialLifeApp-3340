72472072f1280e42ac4a3d82fa0c0dcf66c38d3a
Tenebr1sm <shadowlord058@gmail.com>
2025-11-14 19:55:14 -0600

    made updates to a few files

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+    """Redirect to login if not logged in, otherwise show home."""
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        return redirect('login')
+
+
+@login_required
+def home(request):
+    """Home page for logged-in users."""
+    return render(request, 'home.html')
